d218b9f6a68ff88ba093a02f69e9701b9a4ac922
mini-hiori <phantombisco@gmail.com>
2021-03-02 02:41:27 +0900

    URLリストもssmから得る(暗号化は不要)

--- get_target_url.py
+++ get_target_url.py
@@ -1,12 +1,15 @@
 from typing import List
+import boto3
 
 
 def get_target_url() -> List[str]:
     """
     取得対象にするrssのURLを返却する
     """
-    url_list = [
-        "https://dev.classmethod.jp/feed",
-        "https://blog.serverworks.co.jp/feed"
-    ]
+    # ssmパラメータストアに@区切りでRSS取得対象URLを改行区切りで突っ込んでおく
+    url_param: str = ssm.get_parameter(
+        Name='RSSURLList'
+    )['Parameter']['Value']
+    url_param = url_param.replace("\r", "")
+    url_list: List[str] = url_param.split("\n")
     return url_list
